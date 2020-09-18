20b403e40779be6c4bc102fd8370e68751e6b9b9
developeralexandresantos <skeeter.182@uninove.edu.br>
2020-09-18 17:49:26 -0300

    changed path api

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/',
-    // baseURL: 'https://cadcliapi.azurewebsites.net/',
+    // baseURL: 'http://localhost:8080/',
+    baseURL: 'https://cadcliapi.azurewebsites.net/',
 });
 
 export default api;
