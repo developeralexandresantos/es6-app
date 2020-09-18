8ee591d78a1e49c57291bae101aa6025909d2735
developeralexandresantos <skeeter.182@uninove.edu.br>
2020-09-18 16:52:48 -0300

    configured server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+// const cors = require('cors');
+
+
+// Iniciando o app
+const app = express();
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+// app.use(cors());
+
+// Iniciando o DB
+// mongoose.connect("mongodb://localhost:27017/nodeapi", { useNewUrlParser: true,  useUnifiedTopology: true });
+// requireDir('./src/models');
+
+
+
+// Rotas
+app.use('/', require('./routes'));
+
+app.listen(8081);
