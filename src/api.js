ffdc2b1cf7e878425e5506939a7644aa7c1ed5a5
Alexandre Santos <alexandresantos@Alexandres-MacBook-Air.local>
2020-11-06 12:53:25 -0300

    Changed server address

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://cadcliapi.azurewebsites.net/',
+    // baseURL: 'https://cadcliapi.azurewebsites.net/',
+    baseURL: 'http://localhost:8080',
 });
 
 export default api;
